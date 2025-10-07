2a5fed7bbe2d940647b90e4d3cc69196bb977f33
Rodrigo Barbosa <rodrigobarbosa@ypcontrol.com>
2025-10-07 11:24:44 -0300

    feat: Criaçcao do microservico de emails

---
+++ EmailService/src/test/java/com/drigobarbosa/EmailService/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drigobarbosa.EmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
