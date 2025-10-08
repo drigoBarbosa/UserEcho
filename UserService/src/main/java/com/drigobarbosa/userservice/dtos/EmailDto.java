67f05fbaab9af3fcb981be0ba9cc76c0115ba718
Rodrigo Barbosa <rodrigobs8157@gmail.com>
2025-10-08 14:51:30 -0300

    feat: Criacao do produtor de mensagens

---
+++ UserService/src/main/java/com/drigobarbosa/userservice/dtos/EmailDto.java
@@ -0,0 +1,18 @@
+package com.drigobarbosa.userservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class EmailDto {
+
+    private UUID userId;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+
+}
